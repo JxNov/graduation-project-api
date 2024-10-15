128d776632dd805520b90199c5c954dc75b4faf6
vdoann <openaivdoan@gmail.com>
2024-10-15 13:56:38 +0700

    #1 - soft delete, trash, restore, force detele student generation

--- routes/v1/index.php
+++ routes/v1/index.php
@@ -10,9 +10,12 @@ Route::prefix('admin')
             ->group(function () {
                 Route::get('/', [GenerationController::class, 'index']);
                 Route::post('/', [GenerationController::class, 'store']);
+                Route::get('/trash', [GenerationController::class, 'trash']);
                 Route::get('/{slug}', [GenerationController::class, 'show']);
                 Route::patch('/{slug}', [GenerationController::class, 'update']);
                 Route::delete('/{slug}', [GenerationController::class, 'destroy']);
+                Route::get('/restore/{slug}', [GenerationController::class, 'restore']);
+                Route::delete('/force-delete/{slug}', [GenerationController::class, 'forceDelete']);
             });
 
     });
