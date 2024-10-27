47579af3de3750b9cf8740af5643964d95465863
vdoann <openaivdoan@gmail.com>
2024-10-27 18:59:47 +0700

    #12 - remove column year in table generations

--- database/migrations/2024_09_22_160814_create_generations_table.php
+++ database/migrations/2024_09_22_160814_create_generations_table.php
@@ -10,9 +10,8 @@ return new class extends Migration
     {
         Schema::create('generations', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 50)->unique();;
+            $table->string('name', 50)->unique();
             $table->string('slug', 70)->unique();
-            $table->integer('year');
             $table->date('start_date');
             $table->date('end_date');
             $table->timestamps();
