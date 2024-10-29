52f94f5c5c770c962789ef75bd4933eb088b21c3
dokien <dokien6543@gmail.com>
2024-10-29 23:16:58 +0700

    #6 - student and student-role

---
+++ app/Http/Resources/StudentResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class StudentResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'username'=>$this->username,
+            'date_of_birth' => $this->date_of_birth,
+            'gender' => $this->gender,
+            'address' => $this->address,
+            'phone_number' => $this->phone_number,
+            'email' => $this->email,
+            // 'password' => $this->password,
+        ];
+    }
+}
