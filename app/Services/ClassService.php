47cd46d77ff2589b7c01cc9eccc611bcdc4b037d
vdoann <openaivdoan@gmail.com>
2024-11-04 08:54:34 +0700

    #5 - unupdate slug on table classes

--- app/Services/ClassService.php
+++ app/Services/ClassService.php
@@ -70,11 +70,6 @@ class ClassService
 
             $data['teacher_id'] = $teacher->id;
 
-            $teacherName = $teacher->name;
-            $teacherSlug = Str::slug($teacherName);
-            $classSlug = Str::slug($data['name']);
-            $data['slug'] = $teacherSlug . '-' . $classSlug;
-
             $class->update($data);
 
             return $class;
