8a04b375e90ddcee4b84fb4e55e4002478ff5510
vdoann <openaivdoan@gmail.com>
2024-11-16 13:41:09 +0700

    #12 - rename table name when rollback

--- database/migrations/2024_09_22_160501_create_user_permissions_table.php
+++ database/migrations/2024_09_22_160501_create_user_permissions_table.php
@@ -33,6 +33,6 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('users_permissions');
+        Schema::dropIfExists('user_permissions');
     }
 };
