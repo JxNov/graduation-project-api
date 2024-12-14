8240c2246b189eadf04a9a13a53c2b78e06e0874
vdoann <openaivdoan@gmail.com>
2024-12-14 13:44:10 +0700

    #154 - student join class by code

--- app/Services/ClassroomService.php
+++ app/Services/ClassroomService.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Services;
 
+use App\Models\AcademicYear;
 use App\Models\Classes;
 use Exception;
 use Illuminate\Support\Facades\Auth;
@@ -21,7 +22,21 @@ class ClassroomService
             if ($user->roles()->where('name', 'teacher')->exists()) {
                 $class->classTeachers()->attach($user->id);
             } elseif ($user->roles()->where('name', 'student')->exists()) {
+                $academicYearClass = DB::table('academic_year_classes')->where('class_id', $class->id)->first();
+                // dd($academicYearClass);
                 $class->students()->attach($user->id);
+                $academicYear = AcademicYear::find($academicYearClass->academic_year_id);
+                $existsGenerationUser = DB::table('user_generations')->where('user_id', $user->id)
+                    ->where('academic_year_id', $academicYear->id)
+                    ->where('generation_id', $academicYear->generation_id)->first();
+
+                if (!$existsGenerationUser) {
+                    DB::table('user_generations')->insert([
+                        'user_id' => $user->id,
+                        'generation_id' => $academicYear->generation_id,
+                        'academic_year_id' => $academicYear->id,
+                    ]);
+                }
             }
 
             return $class;
