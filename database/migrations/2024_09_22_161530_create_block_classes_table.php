bb5b5530bcad5bf331efad27ff5671333be4d091
vdoann <openaivdoan@gmail.com>
2024-12-17 23:36:08 +0700

    #12 - database

--- database/migrations/2024_09_22_161530_create_block_classes_table.php
+++ database/migrations/2024_09_22_161530_create_block_classes_table.php
@@ -9,8 +9,13 @@ return new class extends Migration {
     {
         Schema::create('block_classes', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('block_id')->constrained('blocks')->onDelete('cascade');
-            $table->foreignId('class_id')->constrained('classes')->onDelete('cascade');
+            $table->foreignId('block_id')
+                ->constrained('blocks')
+                ->onDelete('cascade');
+                
+            $table->foreignId('class_id')
+                ->constrained('classes')
+                ->onDelete('cascade');
             $table->timestamps();
             $table->softDeletes();
 
