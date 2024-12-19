4f672840a6c74f38196553d249c87da013339dd1
vdoann <openaivdoan@gmail.com>
2024-12-19 16:25:25 +0700

    #5 - change response semester

--- app/Http/Resources/SemesterResource.php
+++ app/Http/Resources/SemesterResource.php
@@ -10,13 +10,16 @@ class SemesterResource extends JsonResource
 {
     public function toArray(Request $request): array
     {
+        $academicYear = optional($this->academicYear);
+
         return [
             'name' => $this->name,
             'slug' => $this->slug,
             'startDate' => Carbon::parse($this->start_date)->format('d/m/Y'),
             'endDate' => Carbon::parse($this->end_date)->format('d/m/Y'),
             'academicYearId' => $this->academic_year_id,
-            'academicYearName' => optional($this->academicYear)->name
+            'generationName' => optional($academicYear->generation)->name,
+            'academicYearName' => $academicYear->name
         ];
     }
 }
